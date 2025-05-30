5a2eb0aec617e4eadc85ac86619b978751a03b55
HaiToan <toan53011@gmail.com>
2025-05-30 11:29:55 +0700

    update

--- app/giao-duc/giao-duc-dac-biet/[...slug]/page.tsx
+++ app/giao-duc/giao-duc-dac-biet/[...slug]/page.tsx
@@ -11,16 +11,26 @@ const page = () => {
   const detail: any = phuongPhap.find((khoahoc) => khoahoc.slug === slug[0])
   return (
     <div>
-      <div className='flex mt-16 xl:flex-row flex-col'>
-        <div className='flex-2/4 bg-[#111827] flex flex-col justify-end pl-[40px] md:pl-[80px] pr-[32px] pb-[56px] gap-[16px]'>
-          <h3 className="text-5xl font-bold text-white mt-10">{detail?.name}</h3>
-          <p className="text-lg font-normal text-white">{detail?.note}</p>
+      <div className='flex mt-16 xl:flex-row flex-col relative'>
+        <div className='flex-2/4 min-h-[400px] bg-[#111827] flex flex-col justify-end pl-[40px] md:pl-[80px] pr-[32px] pb-[56px] gap-[16px]'>
+          <h3 className="text-5xl font-bold text-white mt-10 xl:hidden block">{detail?.name}</h3>
+          <p className="text-lg font-normal text-white xl:hidden block">{detail?.note}</p>
         </div>
         <div className='flex-2/4'>
-          <img src={detail?.image} className="h-full w-full object-cover" alt='asdas' />
+          <img src={detail?.image} className="h-full w-full object-cover max-h-[600px]" alt='asdas' />
+        </div>
+        <div className="absolute max-w-7xl w-full xl:flex hidden bottom-14 left-1/2 pl-4 -translate-x-1/2 ">
+
+          <div className="flex-2/4 pr-5">
+            <h3 className="text-5xl font-bold text-white mt-10">{detail?.name}</h3>
+            <p className="text-lg font-normal text-white mt-4">{detail?.note}</p>
+          </div>
+          <div className="flex-2/4">
+          </div>
         </div>
       </div>
-      <div className="max-w-[846px] mx-auto py-[56px] px-4" dangerouslySetInnerHTML={{__html: detail?.descript}}>
+
+      <div className="max-w-[846px] mx-auto py-[56px] px-4" dangerouslySetInnerHTML={{ __html: detail?.descript }}>
       </div>
     </div>
   )
--- app/thong-tin/cau-chuyen/[...slug-story]/page.tsx
+++ app/thong-tin/cau-chuyen/[...slug-story]/page.tsx
@@ -8,15 +8,22 @@ export default async function Page({ params }: { params: Params }) {
   return (
     <div className="min-h-screen bg-gray-50 pt-20 pb-16">
       <div className="mb-8">
-        <div className="md:h-[600px] flex flex-col md:flex-row">
-          <div className="basis-1/2 bg-[#111827] px-14 flex flex-col justify-end items-center">
-            <div className="text-6xl text-white font-bold mb-10 max-w-4xl mt-10">
-              {newDetail?.title}
-              <div className="text-lg text-white font-normal mt-3">{newDetail?.description}</div>
-            </div>
+        <div className='flex xl:flex-row flex-col relative'>
+          <div className='flex-2/4 min-h-[400px] bg-[#111827] flex flex-col justify-end pl-[40px] md:pl-[80px] pr-[32px] pb-[56px] gap-[16px]'>
+            <h3 className="text-5xl font-bold text-white mt-10 xl:hidden block">{newDetail?.title}</h3>
+            <p className="text-lg font-normal text-white xl:hidden block">{newDetail?.description}</p>
           </div>
-          <div className="basis-1/2">
-            <Image src={newDetail?.img ?? ""} alt="" fill className="object-cover !relative" />
+          <div className='flex-2/4'>
+            <img src={newDetail?.img} className="h-full w-full object-cover  max-h-[600px]" alt='asdas' />
+          </div>
+          <div className="absolute max-w-7xl w-full xl:flex hidden bottom-14 left-1/2 pl-4 -translate-x-1/2 ">
+
+            <div className="flex-2/4 pr-5">
+              <h3 className="text-5xl font-bold text-white mt-10">{newDetail?.title}</h3>
+              <p className="text-lg font-normal text-white mt-4">{newDetail?.description}</p>
+            </div>
+            <div className="flex-2/4">
+            </div>
           </div>
         </div>
       </div>
